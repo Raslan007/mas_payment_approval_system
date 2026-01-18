1945f3a6f1f81d1821283d158cf958309094707d
Raslan007 <ahmedraslan9777@gmail.com>
2026-01-18 17:51:07 +0200

    Add Alembic scaffolding for Flask-Migrate (#119)

---
+++ migrations/env.py
@@ -0,0 +1,64 @@
+from __future__ import with_statement
+
+import logging
+from logging.config import fileConfig
+
+from alembic import context
+from flask import current_app
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+logger = logging.getLogger("alembic.env")
+
+
+def get_engine():
+    try:
+        return current_app.extensions["migrate"].db.get_engine()
+    except (TypeError, AttributeError):
+        return current_app.extensions["migrate"].db.engine
+
+
+def get_database_url() -> str:
+    url = current_app.config.get("SQLALCHEMY_DATABASE_URI")
+    if not url:
+        raise RuntimeError("SQLALCHEMY_DATABASE_URI is not configured.")
+    config.set_main_option("sqlalchemy.url", url)
+    return url
+
+
+target_metadata = current_app.extensions["migrate"].db.metadata
+
+
+def run_migrations_offline():
+    url = get_database_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = get_engine()
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
